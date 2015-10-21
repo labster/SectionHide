d1a866ca5ef64fb6fe97b8db16fe68015c69dffe
Southparkfan <southparkfan223@hotmail.com>
2015-10-21 20:30:28 +0200

    Initial commit

---
+++ SectionHide.alias.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for extension SectionHide (aliases).
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English
+ * @author Simon Oliver (Hoggle42)
+ */
+$aliases['en'] = array(
+	'SectionHide' => array( 'SectionHide' ),
+);
